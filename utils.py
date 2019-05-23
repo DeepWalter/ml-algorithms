1aa3ac476a58bf8c968a0fd50423de9827014ff0
DeepWalter <deepwalter.cn@gmail.com>
2019-05-23 21:57:59 +0800

    Add some utility functions.

---
+++ utils.py
@@ -0,0 +1,44 @@
+"""Collection of utility functions."""
+
+
+import numpy as np
+
+
+def sigmoid(x):
+    """Compute sigmoid element-wise.
+
+    The sigmoid of a scalar x is 1 / (1 + e^{-x}).
+
+    Parameter
+    ---------
+    x: ndarray or scalar
+        input
+    Returns
+    -------
+    ndarray or scalar, same shape of input x
+        element-wise sigmoid of the input
+    """
+    return 1 / (1 + np.exp(-x))
+
+
+def softmax(x, axis=None):
+    """Compute the softmax of array elements along a given axis.
+
+    Parameter
+    ---------
+    x: ndarray
+        input array
+    axis: None or int or tuple of ints, optional
+        axis or axes along which the softmax is performed. The default,
+        axis=None, will perform the softmax over all elements. If axis
+        is negative it counts from the last to the first axis. If axis
+        is a tuple of ints, the softmax is performed on all of the axes
+        specified in tuple.
+
+    Returns
+    -------
+    ndarray, same shape of input x
+        softmax of the input
+    """
+    exp_x = np.exp(x)
+    return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
